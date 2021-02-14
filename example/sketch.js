3d31d78b7eb885bdb4dbe80f3be7341267e5b916
Jon Witts <jonwitts@gmail.com>
2021-02-14 15:46:03 +0000

    Restructure folders

--- example/sketch.js
+++ example/sketch.js
@@ -5,3 +5,10 @@ function setup() {
 function draw() {
   background(220);
 }
+
+function keyReleased() {
+  if (key == 's' || key == 'S') { // save png if S key released
+    let timeStamp = year() + "-" + month() + "-" + day() + "-" + hour() + "-" + minute() + "-" + second() + "-" + nf(millis(), 3, 0);
+    saveCanvas(timeStamp, 'png');
+  }
+}
